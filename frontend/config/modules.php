32262720428140de29ac7c4deafa8248f4173ef8
Vitaliy <theyamshikov@yandex.ru>
2019-05-04 00:16:35 +0600

    fix: return module definition

--- frontend/config/modules.php
+++ frontend/config/modules.php
@@ -4,7 +4,7 @@ use yii2tool\restclient\web\helpers\RestModuleHelper;
 
 $config = [
 	'error' => 'yii2module\error\module\Module',
-	//'user' => 'yii2module\account\module\Module',
+	'user' => 'yii2module\account\module\Module',
 	'welcome' => 'yii2module\dashboard\web\Module',
     'guide' => 'yii2module\guide\module\Module',
 ];

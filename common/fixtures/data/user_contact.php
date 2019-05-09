45b0f63a22a5c06cc8d6274d954abc097ec890f1
Vitaliy <theyamshikov@yandex.ru>
2019-05-09 22:42:57 +0600

    refacto

--- common/fixtures/data/user_contact.php
+++ common/fixtures/data/user_contact.php
@@ -3,7 +3,7 @@
 return [
 	[
 		'id' => 1,
-		'login_id' => 1,
+		'identity_id' => 1,
 		'type' => 'phone',
 		'data' => '77771111111',
 		'is_main' => 1,
@@ -11,7 +11,7 @@ return [
 	],
 	[
 		'id' => 2,
-		'login_id' => 1,
+		'identity_id' => 1,
 		'type' => 'email',
 		'data' => 'admin@example.com',
 		'is_main' => 1,
@@ -19,7 +19,7 @@ return [
 	],
 	[
 		'id' => 3,
-		'login_id' => 2,
+		'identity_id' => 2,
 		'type' => 'phone',
 		'data' => '77771111112',
 		'is_main' => 1,
@@ -27,7 +27,7 @@ return [
 	],
 	[
 		'id' => 4,
-		'login_id' => 3,
+		'identity_id' => 3,
 		'type' => 'phone',
 		'data' => '77771111113',
 		'is_main' => 1,

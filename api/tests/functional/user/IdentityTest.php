3f9ff1904e058c8971d07dc936682ff38e9a46f8
Vitaliy <theyamshikov@yandex.ru>
2019-05-11 23:54:59 +0600

    test: upgrade identity test

--- api/tests/functional/user/IdentityTest.php
+++ api/tests/functional/user/IdentityTest.php
@@ -3,6 +3,7 @@
 namespace tests\functional\user;
 
 use api\tests\schemas\UserSchema;
+use yii2lab\test\enums\TypeEnum;
 use yii2lab\test\Test\BaseActiveApiTest;
 use yii2module\account\domain\v3\helpers\test\AuthTestHelper;
 
@@ -16,6 +17,7 @@ class IdentityTest extends BaseActiveApiTest
 	{
 		AuthTestHelper::authByLogin('admin');
 		$this->readCollection('identity', [], UserSchema::$identity);
+		
 	}
 	
 	public function testAllByLogin()
@@ -24,9 +26,31 @@ class IdentityTest extends BaseActiveApiTest
 		$this->readCollection('identity', ['login' => 'admin'], UserSchema::$identity, 1);
 	}
 	
+	public function testAllByLoginWithRelations()
+	{
+		AuthTestHelper::authByLogin('admin');
+		$actual = $this->readCollection('identity', ['login' => 'admin', 'expand' => 'contacts,roles'], UserSchema::$identity);
+		$this->tester->assertCollectionType([
+			'roles' => TypeEnum::ARRAY,
+			'contacts' => TypeEnum::ARRAY,
+		], $actual);
+	}
+	
 	public function testView()
 	{
 		AuthTestHelper::authByLogin('admin');
-		$this->readEntity('identity', 2, UserSchema::$identity);
+		$actual = $this->readEntity('identity', 1, UserSchema::$identity);
+		$this->tester->assertEmpty($actual['roles']);
+		$this->tester->assertEmpty($actual['contacts']);
+	}
+	
+	public function testViewWithRelations()
+	{
+		AuthTestHelper::authByLogin('admin');
+		$actual = $this->readEntity('identity', 1, UserSchema::$identity, ['expand' => 'contacts,roles']);
+		$this->tester->assertArrayType([
+			'roles' => TypeEnum::ARRAY,
+			'contacts' => TypeEnum::ARRAY,
+		], $actual);
 	}
 }
